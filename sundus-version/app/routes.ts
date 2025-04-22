6f140f92cf7021d4cb76b57fa97b4ee5bf3646c6
sundus-hannah <sundus.tosun15@gmail.com>
2025-04-22 18:29:30 -0500

    Added sundus-hannah's budget-wallet project as sundus-version folder

--- sundus-version/app/routes.ts
+++ sundus-version/app/routes.ts
@@ -2,5 +2,9 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("login", "./login/login.tsx")
+    route("login", "./login/login.tsx"),
+    route("reset", "./login/reset.tsx"), 
+    route("income", "./login/income.tsx"),
+    route("expense", "./login/expense.tsx"),
+
 ] satisfies RouteConfig;
